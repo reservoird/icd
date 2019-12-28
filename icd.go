d32ecfed7cca9ab20c3a7b1a2fb5755534449700
Deepak Kaul <deepak.b.kaul@gmail.com>
2019-12-27 22:06:52 -0500

    updating icd to include name and if the queue is closed

--- icd.go
+++ icd.go
@@ -6,15 +6,18 @@ import (
 
 // Queue provides interface for queue plugins
 type Queue interface {
+	Name() string
 	Config(string) error
 	Push(interface{}) error
 	Pop() (interface{}, error)
+	Closed() bool
 	Close() error
 }
 
 // Ingester provides interface for plugins that ingest (push) data into reservoird
 // struct channel and wait group are for graceful shutdown of ingester plugin
 type Ingester interface {
+	Name() string
 	Config(string) error
 	Ingest(Queue, <-chan struct{}, *sync.WaitGroup) error
 }
@@ -22,6 +25,7 @@ type Ingester interface {
 // Digester provides interface for plugins that filter/annotate (push/pop) data within reservoird
 // struct channel and wait group are for graceful shutdown of digester plugin
 type Digester interface {
+	Name() string
 	Config(string) error
 	Digest(Queue, Queue, <-chan struct{}, *sync.WaitGroup) error
 }
@@ -29,6 +33,7 @@ type Digester interface {
 // Expeller provides interface for plugins that expel (pop) data outof reservoird
 // struct channel and wait group are for graceful shutdown of expeller plugin
 type Expeller interface {
+	Name() string
 	Config(string) error
 	Expel([]Queue, <-chan struct{}, *sync.WaitGroup) error
 }
